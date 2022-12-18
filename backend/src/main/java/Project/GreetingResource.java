3acffca85db499eaf0d62b0cf79a106c00ee7ae3
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2022-12-18 14:25:23 -0300

    Revert "Update Resources"
    This reverts commit 3c38a98d0082a4f1b6e59c501733cf0a6cfebc59.

--- backend/src/main/java/Project/GreetingResource.java
+++ backend/src/main/java/Project/GreetingResource.java
@@ -4,19 +4,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
 
 @Path("/hello")
 public class GreetingResource {
 
-    public List<String> teste = new ArrayList<>();
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public Response hello() {
-        teste.add("Teste");
-        return Response.ok(teste).build();
+    public String hello() {
+        return "Hello RESTEasy";
     }
 }
